1fc01a388fd0765318ebe585161046395d1decae
Marcian Diamond <marciandmnd@yahoo.com>
2018-12-11 17:53:54 -0800

    Types and Data example

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express');
 const app = express();
 const graphqlHttp = require('express-graphql');
 const { buildSchema }= require('graphql');
+
+const events = [];
+
 app.use(express.json());
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -21,11 +39,18 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString()
+      };
+      events.push(event);
+      return event;
     }
   },
   graphiql: true
