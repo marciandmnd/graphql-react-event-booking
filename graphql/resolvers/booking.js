6363be0789bd06083d117d625da616f1d4d3c1ca
Marcian Diamond <marciandmnd@yahoo.com>
2019-01-10 19:04:23 -0800

    login resolver

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -29,7 +29,6 @@ module.exports = {
       const event = transformEvent(booking.event);
       await Booking.deleteOne({_id: args.bookingId});
       return event;
-
     } catch (err  ) {
       throw err;
     }
